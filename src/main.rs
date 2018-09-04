96bc13cc5f6e48401ff6488cd530be51e23d4725
Erlimar Silva Campos <erlimar.campos@squadra.com.br>
2018-09-04 19:09:21 -0300

    Adiciona suporte a PORT

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate nickel;
 
+use std::env;
+
 use nickel::Nickel;
 
 fn main() {
@@ -12,8 +14,14 @@ fn main() {
         }
     });
 
-    match server.listen("127.0.0.1:8080") {
-        Ok(_) => println!("Servidor iniciado!"),
-        Err(_) => eprintln!("Erro ao iniciar o servidor em 127.0.0.1:8080!"),
-    }
+    match env::var("PORT") {
+        Ok(port) => match server.listen(format!("127.0.0.1:{}", port)) {
+            Ok(_) => println!("Servidor iniciado na porta {}!", port),
+            Err(_) => eprintln!("Erro ao iniciar o servidor na porta {}!", port),
+        },
+        Err(e) => match server.listen("127.0.0.1:8080") {
+            Ok(_) => println!("Servidor iniciado na porta padrão 8080!"),
+            Err(_) => eprintln!("Erro ao iniciar o servidor na porta padrão 8080!"),
+        },
+    };
 }
